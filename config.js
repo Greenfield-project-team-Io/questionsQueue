b2e0d6cfd26cb5a2da8cd3353a2ad848b9c39567
Debasish Mozumder <debmozumder@Debasishs-MacBook-Air.local>
2017-04-28 11:11:45 -0400

    minor changes to db config

---
+++ config.js
@@ -0,0 +1,8 @@
+// session and passport config
+exports.secret = 'impenetrable security';
+exports.githubID = 'dc2d3707f3f9edeb958a';
+exports.githubSecret = '0f923f8c49bd504e21d8cd510bfb08ea9ae30462';
+
+// db credentials
+exports.user = 'io';
+exports.password = 'question123';
