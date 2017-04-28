670cbd812ccd01f6d1770eae35413a15bb6d933c
David H <davidhamberlin@gmail.com>
2017-04-28 09:53:55 -0400

    nothing of significance

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
 
 import QueueComponent from './QueueComponent.jsx';
 import QuestionFormComponent from './QuestionFormComponent.jsx';
