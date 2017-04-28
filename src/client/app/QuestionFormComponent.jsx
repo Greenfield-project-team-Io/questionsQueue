2a770c2ecfaecddb1fa618308130a3d499643a27
Leo Gomez <leogomez@gmail.com>
2017-04-27 23:24:38 -0400

    Lay out basic submission form

--- src/client/app/QuestionFormComponent.jsx
+++ src/client/app/QuestionFormComponent.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
 
-const QuestionFormComponent = function (props) {
-  return <div>This is a form component</div>;
+class QuestionFormComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      questionText: ''
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    alert(this.state.questionText);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <textarea name="questionText" onChange={this.handleInputChange} />
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
 
 export default QuestionFormComponent;
