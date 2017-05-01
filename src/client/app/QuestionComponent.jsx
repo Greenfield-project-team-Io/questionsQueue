28acf50c0472d631cc938988d3fdef0dca2d299f
David H <davidhamberlin@gmail.com>
2017-05-01 10:22:19 -0400

    implement delete button

--- src/client/app/QuestionComponent.jsx
+++ src/client/app/QuestionComponent.jsx
@@ -9,6 +9,7 @@ const QuestionComponent = (props) => {
       <div>Asked on {Date(question.createdAt)}</div>
       <button onClick={() => props.handleUpvote(question)}>Vote</button>
       <button onClick={() => props.handleAnswered(question)}>Clear</button>
+      <button onClick={() => props.handleDelete(question)}>Delete</button>
     </div>
   );
 };
