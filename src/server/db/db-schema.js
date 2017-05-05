9dcf709c5586aafd100fbe95a62d7fad4a1fb22a
Leo Gomez <leogomez@gmail.com>
2017-05-05 12:29:30 -0400

    Add username and usersvoted fields to question schema

--- src/server/db/db-schema.js
+++ src/server/db/db-schema.js
@@ -2,9 +2,11 @@ const db = require('./db-config');
 const mongoose = require('mongoose');
 
 const questionSchema = new mongoose.Schema({
-  questionText: String,
+  questionText: { type: String, required: true },
   votes: Number,
   answered: Boolean,
+  username: { type: String, required: true },
+  usersVoted: [String],
   // timestamps: { createdAt: 'created_at' },
 });
 
