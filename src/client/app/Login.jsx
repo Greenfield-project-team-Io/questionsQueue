bb329b37060fc0ffa5fa8dd4ad1070f79761e540
Debasish Mozumder <debmozumder@Debasishs-Air.fios-router.home>
2017-05-05 17:04:28 -0400

    working on login page

--- src/client/app/Login.jsx
+++ src/client/app/Login.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import FlatButton from 'material-ui/FlatButton';
 
 // Maybe we shouldn't check if they're logged in here. This could be done in the router.
 const LoginComponent = props => (
   props.loggedIn ? (
     <Redirect to="/questions" />
   ) : (
-    <a href="/auth/github">
-      Log in with github
-    </a>
-    // <button onClick={() => {
-    //   props.login(() => {});
-    // }}>Log In {props.name}</button>
+    <MuiThemeProvider>
+      <div styles= "background-color: blue">
+        <AppBar title="Question Queue" showMenuIconButton={false}
+          iconElementRight={
+            <FlatButton label="Log In" href="/auth/github"/>
+          }
+          />
+        </div>
+      </MuiThemeProvider>
   )
 );
 export default LoginComponent;
