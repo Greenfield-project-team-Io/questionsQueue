2c8fb3cf300ebd04237494505bea5a64f0d20db7
Debasish Mozumder <debmozumder@Debasishs-Air.fios-router.home>
2017-05-05 20:48:22 -0400

    loginPage created with proper style and color

--- src/client/app/Login.jsx
+++ src/client/app/Login.jsx
@@ -1,23 +1,33 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
 import FlatButton from 'material-ui/FlatButton';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 // Maybe we shouldn't check if they're logged in here. This could be done in the router.
+
+// var divStyle = {
+//   resizeMode: 'stretch',
+//   // WebkitTransition: 'all', // note the capital 'W' here
+//   // msTransition: 'all' // 'ms' is the only lowercase vendor prefix
+// };
+// <img src='../images/hack-reactor-logo.png'></img>
+  // <a href="/auth/github" class="button">Sign In</a>
+  // <p>You are not signed in.</p>
 const LoginComponent = props => (
   props.loggedIn ? (
     <Redirect to="/questions" />
   ) : (
-    <MuiThemeProvider>
-      <div styles= "background-color: blue">
-        <AppBar title="Question Queue" showMenuIconButton={false}
-          iconElementRight={
-            <FlatButton label="Log In" href="/auth/github"/>
-          }
-          />
-        </div>
+    <div>
+      <MuiThemeProvider>
+      <AppBar title="Question Queue" showMenuIconButton={false}
+        iconElementRight={
+          <FlatButton label="Log In" href="/auth/github"/>
+        }
+      />
       </MuiThemeProvider>
+        <img src='../images/loginBack.png'></img>
+    </div>
   )
 );
 export default LoginComponent;
